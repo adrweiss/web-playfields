b2e805b7f28329951722c5360a53936045560993
Adrian Weiss <weiss.adrian@outlook.com>
2020-11-29 00:42:59 +0100

    add success and error messages

--- frontend/src/NewUsr.js
+++ frontend/src/NewUsr.js
@@ -1,35 +1,76 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 import './NewUsr.css';
 import axios from './axios';
-
+import Banner from 'react-js-banner';
 
 function NewUsr() {
+  const [response, setResponse] = useState([]);
+
+
   const handleClick = () => {
     var username = document.getElementById('username').value
     var password_1 = document.getElementById('pass').value
     var password_2 = document.getElementById('pass_again').value
     
+
     if (password_1 === password_2) {
+      
       axios.post('/users', {
           'email': username,
           'pword': password_1
         })
         .then((response) => {
-          console.log(response);
+          setResponse('The user was successfull created!')
         }, (error) => {
-          console.log(error);
+          console.log(error)
+          setResponse('It occured a unknown error!')
         });
     } else {
-      alert('the passwords does mot match!')
+      setResponse('The passwords does mot match!')
     }
   }
 
   return (
     <div className='container'>   
-      <div className='create__container'>
+      <div className='create__container'>        
         <h1>Create New User</h1>
         
+        {response === 'The passwords does mot match!' ? (
+          <Banner 
+            css={{color: "#FFF", backgroundColor: "red", borderRadius: '8px', fontFamily: 'Fira Sans', fontSize: 18}}
+            title={response} 
+            showBanner='true'
+          />
+        ) : (
+        <Banner 
+            showBanner='false'        
+        />)}
+
+        {response === 'The user was successfull created!' ? (
+          <Banner 
+            css={{color: "#FFF", backgroundColor: "green", borderRadius: '8px', fontFamily: 'Fira Sans', fontSize: 18}}
+            title={response} 
+            showBanner='true'
+          />
+        ) : (
+        <Banner 
+            showBanner='false'        
+        />)}
+        
+        {response === 'It occured a unknown error!' ? (
+          <Banner 
+            css={{color: "#FFF", backgroundColor: "red", borderRadius: '8px', fontFamily: 'Fira Sans', fontSize: 18}}
+            title={response} 
+            showBanner='true'
+          />
+        ) : (
+        <Banner 
+            showBanner='false'        
+        />)}
+
+        
+
         <div>
             <label className='create__input__label'>E-Mail:</label>
             <input className='create__input__field' type="text" id="username" name="username"/>
