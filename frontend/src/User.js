f369308fca284ddf205aa9b477f4230c7e18a953
Adrian Weiss <weiss.adrian@outlook.com>
2020-11-28 21:28:02 +0100

    interface for password and nickname change

--- frontend/src/User.js
+++ frontend/src/User.js
@@ -1,16 +1,73 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+import './User.css'
+import DeleteIcon from '@material-ui/icons/Delete';
+
 
 function User() {
+  const currentNickname = 'TestName'
+
+  const rights = [
+    {
+      
+    }
+  ]
+
   return (
-    <div>
-      <h1>
-        User-Self-Service-Page      
-      </h1>
-      <li>Delete</li>
-      <li>old password</li>
-      <li>new password</li>
-      <li>new password again</li>
-      <button>Accept new password</button>
+    <div> 
+    <div className='container'>   
+        <div className='user__change__container'>
+          <h1>User-Self-Service-Page</h1> 
+          {`Here you can change your nickname. Your current nickname is ${currentNickname}. Your nickname must be unique.`}
+          <div>
+              <label className='user__input__label'>New nickname</label>
+              <input className='user__input__field' type="text" id="username" name="username"/>
+          </div>
+          <div className="nickname__change">
+            <Button className='accept__new__nickname' variant="contained" color="primary" disableElevation>
+              Accept 
+            </Button>
+          </div>
+          
+          <div className='change__password__section'>
+          {`Here you can change your current password.`}
+            <label className='user__input__label__pw'>Old password:</label>
+            <input className='user__input__field__pw' type="password" id="pass" name="password"
+              minlength="8" required/>
+          </div>
+
+          <div>
+            <label className='user__input__label__pw'>New Password:</label>
+            <input className='user__input__field__pw' type="password" id="pass" name="password"
+              minlength="8" required/>
+          </div>
+          
+          <div>
+            <label className='user__input__label__pw'>New Password again:</label>
+            <input className='user__input__field__pw' type="password" id="pass" name="password"
+              minlength="8" required/>
+          </div>
+          
+
+          <Button className='accept__new__password' variant="contained" color="primary" disableElevation>
+            Accept new password
+          </Button>
+          
+          <div className='delete__profile__Section'>
+            <div className='delete__profile__Section__v2'>
+              <Button className='delete__new__password' variant="contained" color="secondary" startIcon={<DeleteIcon />}>
+                Delete Profile
+              </Button>
+            </div>
+          </div>
+          
+        </div>
+      </div>
+      <div className='container'>   
+        <div className='user__rights__container'>
+          <h1>The User will be able to see all his rights on this screen</h1> 
+        </div>
+      </div>
     </div>
   )
 }
