3c30564ce5aaee10715028fde194970eb4749f0a
Adrian Weiss <weiss.adrian@outlook.com>
2020-11-28 18:13:03 +0100

    add Posts on Homescreen

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,21 +1,77 @@
 import React from 'react'
+import './Home.css'
+import Message from './Message'
+import Pagination from '@material-ui/lab/Pagination';
+import Grid from '@material-ui/core/Grid';
+
 
 function Home() {
+  const messageFlow = [
+    {
+      "id": "1234", 
+      "title": "A-title",
+      "post":"first message ",
+      "usr":"A",
+      "timestamp":"28. November"
+      },
+      {
+      "id": "5678", 
+      "title": "B-title",
+      "post":"second message ",
+      "usr":"B",
+      "timestamp":"29. November"
+      },
+      {
+      "id": "91011", 
+      "title": "C-title",
+      "post":"third message ",
+      "usr":"C",
+      "timestamp":"30. November"
+      }
+  ]
+
+  
+
+  const numberPages = 15
+  
+  const handleChangePage = (event, newPage) => {
+    console.log(newPage)    
+  }
+
+
   return (
-    <div>
-        <h1>The start screen will be placed here/ It will be possible to add posts here.</h1>
-        <li>Add new Post</li>
-        <li>Delete Post</li>
-        <li>Show Post</li>
-        <li>Timestamp</li>
-        <li>Name</li>
-
-        <div className='message'>
-          <li>User</li>
-          <li>Message</li>
-          <li>Timestampt</li>
+    <div> 
+        <div className='homescreen'>
+          <h1>The start screen will be placed here/ It will be possible to add posts here.</h1>
         </div>
-
+        <div className="homescreen">                 
+                <textarea className="input__write__post"  id="text" name="text" cols="35" rows="4" maxlength="100">Write a new Post</textarea> 	
+                <input type="submit" value="Send" />  
+        </div>
+        <div>
+          {messageFlow?.length === 0 ? (
+            <div className='homescreen'>
+              <h2>No post available</h2> 
+            </div>
+          ):(
+            <div>    
+              {messageFlow?.map(item=> (
+                <Message
+                id={item.id}
+                title={item.title}
+                post={item.post}
+                usr={item.usr}
+                timestamp={item.timestamp}
+                />
+              ))}      
+            </div>
+          )}
+        </div>
+      
+        <Grid container justify = "center">
+          <Pagination className="page__number" count={numberPages} variant="outlined" shape="rounded" onChange={handleChangePage}/>
+        </Grid>
+        
     </div>
   )
 }
--- frontend/src/Message.js
+++ frontend/src/Message.js
@@ -1,13 +1,32 @@
-import React from 'react'
-import "."
+import React from 'react';
+import "./Message.css";
+import Button from '@material-ui/core/Button';
 
-function message() {
+function Message({id, title, post, usr, timestamp}) {
   return (
-    <div>
+    <div className='container'>
+      <div className='post__container'>
+        
+        <h3 className='post__title'>{title}</h3>
       
+        {post}
+        <div className='user__date_container'>
+          <div>
+            User: {usr} 
+          </div>
+          <div>
+            Date: {timestamp}
+          </div>
+        </div>
+        <div className='delete_button' >
+          <Button size="small" variant="contained" color="secondary">
+            Delete
+          </Button>
+        </div>
+      </div>
     </div>
   )
 }
 
 
-export default message
+export default Message
