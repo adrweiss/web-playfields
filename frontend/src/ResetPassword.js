dbf9d18530e48be8918285665b7637605ef45068
Adrian Weiss <weiss.adrian@outlook.com>
2020-11-28 19:08:30 +0100

    add layout reset password

--- frontend/src/ResetPassword.js
+++ frontend/src/ResetPassword.js
@@ -1,11 +1,23 @@
 import React from 'react'
+import './ResetPassword.css'
+import Button from '@material-ui/core/Button';
+
 
 function ResetPassword() {
   return (
-    <div>
-      <h1>Reset Password here</h1>
-      <li>E-Mail</li>
-      <button>Send</button>
+    <div className='container'>
+      <div className='reset__container'>
+        <h1>Reset Password here</h1>
+        
+        <div>
+            <label className='reset__input__label'>Username:</label>
+            <input className='reset__input__field' type="text" id="username" name="username"/>
+        </div>
+        
+        <Button className='reset__button' variant="contained" color="primary" disableElevation>
+          Send
+        </Button>
+      </div>
     </div>
   )
 }
