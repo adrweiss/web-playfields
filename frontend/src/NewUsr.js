2308639f68d5c03e86adabac1bca773c62f0e8a9
Adrian Weiss <weiss.adrian@outlook.com>
2020-11-28 19:23:42 +0100

    create User side changed

--- frontend/src/NewUsr.js
+++ frontend/src/NewUsr.js
@@ -1,13 +1,34 @@
 import React from 'react'
+import Button from '@material-ui/core/Button';
+import './NewUsr.css'
 
 function NewUsr() {
   return (
-    <div>
-      <h1>Create New User</h1>
-      <li>E-Mail</li>
-      <li>Password</li>
-      <li>Password again</li>
-      <button>Create</button>
+    <div className='container'>   
+      <div className='create__container'>
+        <h1>Create New User</h1>
+        
+        <div>
+            <label className='create__input__label'>E-Mail:</label>
+            <input className='create__input__field' type="text" id="username" name="username"/>
+        </div>
+
+        <div>
+          <label className='create__input__label'>Password:</label>
+          <input className='create__input__field' type="password" id="pass" name="password"
+            minlength="8" required/>
+        </div>
+
+        <div>
+          <label className='create__input__label'>Password again:</label>
+          <input className='create__input__field' type="password" id="pass" name="password"
+            minlength="8" required/>
+        </div>
+        
+        <Button className='create__button' variant="contained" color="primary" disableElevation>
+          Create
+        </Button>
+      </div>
     </div>
   )
 }
