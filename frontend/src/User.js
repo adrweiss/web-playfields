e7be03c90da702824b3b7a54525f22a790ce0425
Adrian Weiss <weiss.adrian@outlook.com>
2020-12-07 23:51:50 +0100

    prepare user view

--- frontend/src/User.js
+++ frontend/src/User.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Button from '@material-ui/core/Button';
 import './User.css'
 import DeleteIcon from '@material-ui/icons/Delete';
 import Grid from '@material-ui/core/Grid';
+
+
 //import Collapse from '@material-ui/core/Collapse';
 
-import RolesRightsRow from './RolesRightsRow'
+//import RolesRightsRow from './RolesRightsRow'
 import TableRow from '@material-ui/core/TableRow';
 import TableCell from '@material-ui/core/TableCell';
 //import Paper from '@material-ui/core/Paper';
@@ -16,8 +18,6 @@ import TableContainer from '@material-ui/core/TableContainer';
 
 
 function User() {
-  const currentNickname = 'TestName'
-
   const roles = [
     {
       "role_id": "234",
@@ -57,11 +57,38 @@ function User() {
     }
   ]
 
+  const currentNickname = 'TestName'
+  const [role, setRole] = useState(roles[0]);
+  const [size, setSize] = useState(8);
+  const [hideRoleInfo, setHideRoleInfo] = useState(true);
+
+
+  //useEffect(() => { setHideRoleInfo(false) }, [])
+
+  const handleCellClick = (event, role_id) => {
+    if (role_id === role.role_id) {
+      setHideRoleInfo(!hideRoleInfo)
+      if (hideRoleInfo) {
+        setSize(4)
+      } else {
+        setSize(8)
+      }
+    } else {
+      if (hideRoleInfo) {
+        setHideRoleInfo(false)
+        setSize(4)
+      }
+    }
+
+    setRole(roles.find(role => role.role_id === role_id))
+  }
+
+
   return (
     <div>
       <h1>User Self Service</h1>
       <Grid container spacing={3}>
-        <Grid item xs={12} sm={6}>
+        <Grid item sm={4}>
           <div className="container__user">
             <h2>Change your user settings</h2>
 
@@ -96,25 +123,57 @@ function User() {
             </div>
           </div>
         </Grid>
-        <Grid item xs={12} sm={6}>
-          <h2>List of your roles and rights</h2>
-          <TableContainer >
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell />
-                  <TableCell align="left">Assigned system roles</TableCell>
-                  <TableCell align="left">Assignemnt date</TableCell>
-                  <TableCell align="left">Role description</TableCell>
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {roles?.map((role) => (
-                 <RolesRightsRow key={role.role_id} role={role}/>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+        <Grid item sm={size}>
+          <div className='table__container'>
+            <h2>List of assigned roles</h2>
+            <TableContainer>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="left">Roles</TableCell>
+                    <TableCell align="left">Date</TableCell>
+                    <TableCell align="left">Description</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {roles?.map((role) => (
+                    <TableRow
+                      hover
+                      key={role.role_id}
+                      onClick={(event) => handleCellClick(event, role.role_id)}
+                    >
+                      <TableCell align="left">{role.role_name}</TableCell>
+                      <TableCell align="left">{role.assignment_date}</TableCell>
+                      <TableCell align="left">{role.role_description}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
+        </Grid>
+        <Grid item sm={4}>
+          <div className='table__container' hidden={hideRoleInfo}>
+            <h2>List of assigned rights for "{role?.role_name}"</h2>
+            <TableContainer>
+              <Table stickyHeader aria-label="sticky table">
+                <TableHead>
+                  <TableRow>
+                    <TableCell align="left">Rights</TableCell>
+                    <TableCell align="left">Description</TableCell>
+                  </TableRow>
+                </TableHead>
+                <TableBody>
+                  {role.rights?.map((right) => (
+                    <TableRow key={right.right_id}>
+                      <TableCell align="left">{right.right_name}</TableCell>
+                      <TableCell align="left">{right.right_description}</TableCell>
+                    </TableRow>
+                  ))}
+                </TableBody>
+              </Table>
+            </TableContainer>
+          </div>
         </Grid>
       </Grid>
     </div>
