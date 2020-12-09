f30969773666d9daa45c6f2c822ead29eee20c3f
Adrian Weiss <weiss.adrian@outlook.com>
2020-12-09 12:16:17 +0100

    test-pip

--- frontend/src/Game.js
+++ frontend/src/Game.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Game() {
   return (
     <div>
-      <h1>This is the place for the game side.</h1>
+      <h1>This is the place for the game side. Tst</h1>
     </div>
   )
 }
