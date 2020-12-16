88bdfa5775e1f720325a8df9f7b724f871873a90
Adrian Weiss <weiss.adrian@outlook.com>
2020-12-16 09:24:13 +0100

    change backend routs

--- frontend/src/services/user.service.js
+++ frontend/src/services/user.service.js
@@ -1,29 +1,10 @@
 import axios from "../axios";
 import authHeader from "./auth-header";
 
-const getPublicContent = () => {
-  return axios.get("test/all");
-};
-
-const getUserBoard = () => {
-  return axios.get("test/user", { headers: authHeader() });
-};
-
-const getModeratorBoard = () => {
-  return axios.get("test/mod", { headers: authHeader() });
-};
-
-const getAdminBoard = () => {
-  return axios.get("test/admin", { headers: authHeader() });
-};
-
-/*const getRolesRights = () => {
+const getRolesRights = () => {
   return axios.get("/usr/rights", { headers: authHeader() });
-};*/
+};
 
 export default { 
-  getPublicContent,
-  getUserBoard,
-  getModeratorBoard,
-  getAdminBoard,
+  getRolesRights,
 };
