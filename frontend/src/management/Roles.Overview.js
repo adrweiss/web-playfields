51a9e7822228fe4d2c912704b6d501850d61b49e
Adrian Weiss <weiss.adrian@outlook.com>
2020-12-21 01:20:29 +0100

    get roles from backend

--- frontend/src/management/Roles.Overview.js
+++ frontend/src/management/Roles.Overview.js
@@ -1,6 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import ManagementRoleService from '../services/mgt.role.service'
+
 
 function RolesOverview() {
+
+  useEffect(() => {
+    ManagementRoleService.getRoles().then((response) => {
+      console.log(response.data)
+    },
+      (error) => {
+        const _content =
+          (error.response &&
+            error.response.data &&
+            error.response.data.message) ||
+          error.message ||
+          error.toString();
+
+        console.log(_content);
+      })
+  }, [])
   return (
     <div>
       {'Test for Role Overview.'}
