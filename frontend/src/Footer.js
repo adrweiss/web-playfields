92dd8bfc63aa64104f72f3fc56e71e81336705f7
Adrian Weiss <weiss.adrian@outlook.com>
2021-01-31 22:14:42 +0100

    add styling report and contact form

--- frontend/src/Footer.js
+++ frontend/src/Footer.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import Modal from 'react-modal';
 import Button from '@material-ui/core/Button';
 
+import TextField from '@material-ui/core/TextField';
+
 Modal.setAppElement('body')
 
 const customStyles = {
@@ -22,7 +24,7 @@ const customStyles = {
 function Footer() {
   const [modalContactForm, setModalContactForm] = useState(false);
   const [modalReportButton, setModalReportButton] = useState(false);
-  
+
 
   const handlerModalContactForm = () => {
     setModalContactForm(!modalContactForm)
@@ -45,22 +47,51 @@ function Footer() {
           Report Bug
         </div>
       </Button>
-      
+
       <div>
         <Link to="/impressum" className='footer__entity'>
           {'Impressum'}
         </Link>
       </div>
-  
+
       <Modal
         isOpen={modalContactForm}
         onRequestClose={handlerModalContactForm}
         style={customStyles}
         contentLabel="Contact_Form"
       >
-        <div>
+        <div className='modal__footer'>
           <h3>Contact Form</h3>
-          <p>Not in place</p>
+
+          <TextField
+            className='modal__footer__textfield'
+            label="Email (ptional)"
+            variant="outlined"
+            margin="normal"
+          />
+
+          <TextField
+            className='modal__footer__textfield'
+            label="Resason"
+            variant="outlined"
+            margin="normal"
+          />
+
+          <TextField
+            className='modal__footer__textfield'
+            label="Description"
+            multiline
+            variant="outlined"
+            margin="normal"
+          />
+
+          <Button
+            className='modal__footer__send'
+            variant="contained"
+            color="primary"
+          >
+            Send
+          </Button>
         </div>
       </Modal>
 
@@ -70,9 +101,31 @@ function Footer() {
         style={customStyles}
         contentLabel="Report_Bug"
       >
-        <div>
+        <div className='modal__footer'>
           <h3>Report Bug</h3>
-          <p>Not in place</p>
+
+          <TextField
+            className='modal__footer__textfield'
+            label="Resason"
+            variant="outlined"
+            margin="normal"
+          />
+
+          <TextField
+            className='modal__footer__textfield'
+            label="Description"
+            multiline
+            variant="outlined"
+            margin="normal"
+          />
+
+          <Button
+            className='modal__footer__send'
+            variant="contained"
+            color="primary"
+          >
+            Send
+          </Button>
         </div>
       </Modal>
     </div>
