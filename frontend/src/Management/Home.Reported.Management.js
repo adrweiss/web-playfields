2f6e5be74dce7142e1e65fb0a94c6be863e87d11
Adrian Weiss <weiss.adrian@outlook.com>
2021-02-17 00:07:22 +0100

    add files for reported posts handling

--- frontend/src/Management/Home.Reported.Management.js
+++ frontend/src/Management/Home.Reported.Management.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import './Home.Reported.Management.css'
 
 function HomeReportedManagement({id, username, body, title, solved, blocked, changed, date}) {
   return (
-    <div>
-      test
+    <div className="reported__posts__container">
+      <h3>{title}</h3>
+      <div>
+        {body}
+      </div>
+      <div>
+        edit delete, block, solved 
+      </div>
+      <div>
+        username, changed, date
+      </div>
     </div>
   )
 }
